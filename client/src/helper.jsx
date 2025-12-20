2c2e623109c961e17cd1f5e135f7a046d5e540b0
shahariar270 <mdshahariar102@gmail.com>
2025-12-20 06:10:38 +0600

    helper array

---
+++ client/src/helper.jsx
@@ -0,0 +1,14 @@
+export const renderList ={
+    todo:[
+        'complete useref',
+        'deep drive on redux',
+        'run actions',
+        'database full crud operations'
+    ],
+    complete:[
+        'all office tasks'
+    ],
+    inProgress:[
+        'testing dnd'
+    ]
+}
