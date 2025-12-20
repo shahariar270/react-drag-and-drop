57fd2c423c5fbd3bb38d458343bfd6610040f3c3
shahariar270 <mdshahariar102@gmail.com>
2025-12-20 06:12:42 +0600

    render ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
 import React from "react"
+import { renderList } from "./helper"
 
 function App() {
 
   return (
     <React.Fragment>
-     
+      <div className="board">
+        {Object.entries(renderList).map(([status, items]) => (
+          <div className="column" key={status}>
+            <h3>{formatTitle(status)}</h3>
+
+            <ul>
+              {items.map((task, index) => (
+                <li key={index}>{task}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </React.Fragment>
   )
 }
